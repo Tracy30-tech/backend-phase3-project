f9b9a966725ce22258300a7306bdb880715657ca
TracyAtieno <tracy.atieno@student.moringaschool.com>
2023-03-01 09:16:45 +0300

    environment-setup

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-# ENV['RACK_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
 
-# # Require in Gems
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['RACK_ENV'])
+# Require in Gems
+require 'bundler/setup'
+Bundler.require(:default, ENV['RACK_ENV'])
 
-# # Require in all files in 'app' directory
-# require_all 'app'
+# Require in all files in 'app' directory
+require_all 'app'
